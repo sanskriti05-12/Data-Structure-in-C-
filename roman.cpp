e298ff04b6a13d4259a47dbc6fe6dac963101127
sanskriti05-12 <ssanskriti172@gmail.com>
2025-03-21 23:39:30 +0530

    update

--- roman.cpp
+++ roman.cpp
@@ -2,5 +2,12 @@
 
 #include<map>
 using namespace std;
+int romantoint(string s)
+{
+    unordered_map<char,int>up{{'I,1'},{'v',5},{'x',10}}
+    
+}
 int main(){
+    string s="XI";
+    cout<<romantoint(s);
 }
