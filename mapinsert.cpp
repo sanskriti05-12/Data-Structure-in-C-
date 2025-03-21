b71428fd159e180a1331a60a5f1d5e9a76b56c99
sanskriti05-12 <ssanskriti172@gmail.com>
2025-03-21 18:04:20 +0530

    update

--- mapinsert.cpp
+++ mapinsert.cpp
@@ -17,22 +17,75 @@
 //     }
     
 
+// // }
+// // occureence of characters
+// #include<iostream>
+
+// #include<map>
+// using namespace std;
+// void occur(string s)
+// {
+// map<char,int>mp;
+// for(int i=0;i<s.size();i++)
+// {
+// mp [s[i]]++; 
+// }
+// for (auto p:mp)
+// {
+//     cout<<p.first<<"="<<p.second<<"\n";
+// }
+// }
+
+// int main(){
+//     string s="wellcome";
+//     occur (s);
+// } 
+//20 th march
+
+//input lena dynamic programming
+
 // }
 // occureence of characters
 #include<iostream>
 
 #include<map>
 using namespace std;
-void occur(string s)
-{
-map<char,int>mp;
-for(int i=0;i<s.size();i++)
-{
-     c      cvcccccccccxccccccccccccc
+int main(){
+    map<int,string>mp;
+    int roll ,n;
+    string name;
+    cout<<"enter how many records";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<"enter roll no";
+        cin>>roll;
+        cout<<"enter name";
+        cin>>name;
+        mp.insert({roll,name});
+
+    }
+    cout<<"\n records are \n";
+    for(auto p:mp)
+    {
+        cout<<p.first<<":"<<p.second<<"\n";
+
+    }
+    cout<<"enter roll no.for searching";
+    cin>>roll;
+     auto k=mp.find(roll);
+    if( k!=mp.end())
+    {
+        // cout<<k->first<<","<<k->second<<"\n";
+        mp.erase(roll);
+    }
+    else
+    {
+        cout<<"not found";
+    }
+
 }
 
-int main(){
-    string s="wellcome";
-    occur (s);
-}    
+ 
+
 
